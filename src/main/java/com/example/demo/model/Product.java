4d0b69ee7d338da72cf19f7ec49a60853568efc7
chous <paul4017boy@gmail.com>
2022-09-06 16:48:07 +0100

    enum refactor

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,11 +1,13 @@
 package com.example.demo.model;
 
+import com.example.demo.constant.ProductCategory;
+
 import java.util.Date;
 
 public class Product {
     private Integer productId;
     private String productName;
-    private String category;
+    private ProductCategory category;
     private String imageUrl;
     private Integer price;
     private Integer stock;
@@ -21,7 +23,7 @@ public class Product {
         return productName;
     }
 
-    public String getCategory() {
+    public ProductCategory getCategory() {
         return category;
     }
 
@@ -57,7 +59,7 @@ public class Product {
         this.productName = productName;
     }
 
-    public void setCategory(String category) {
+    public void setCategory(ProductCategory category) {
         this.category = category;
     }
 
--- src/main/java/com/example/demo/rowmapper/ProductRowMapper.java
+++ src/main/java/com/example/demo/rowmapper/ProductRowMapper.java
@@ -1,6 +1,7 @@
 package com.example.demo.rowmapper;
 
 
+import com.example.demo.constant.ProductCategory;
 import com.example.demo.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -15,7 +16,11 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(rs.getInt("product_id"));
         product.setProductName(rs.getString("product_name"));
-        product.setCategory(rs.getString("category"));
+
+        String categoryStr = rs.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+
         product.setImageUrl(rs.getString("image_url"));
         product.setPrice(rs.getInt("price"));
         product.setStock(rs.getInt("stock"));
