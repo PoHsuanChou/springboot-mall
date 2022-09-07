98ed6db32d9a83a74309a705cc09c8b1f9c54acb
chous <paul4017boy@gmail.com>
2022-09-08 00:11:06 +0100

    search列表跟條件

--- src/main/java/com/example/demo/dao/impl/ProductDaoImp.java
+++ src/main/java/com/example/demo/dao/impl/ProductDaoImp.java
@@ -1,5 +1,6 @@
 package com.example.demo.dao.impl;
 
+import com.example.demo.constant.ProductCategory;
 import com.example.demo.dao.ProductDao;
 import com.example.demo.dto.ProductRequest;
 import com.example.demo.model.Product;
@@ -23,10 +24,20 @@ public class ProductDaoImp implements ProductDao {
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
-    public List<Product> getProducts() {
+    public List<Product> getProducts(ProductCategory category,String search) {
         String sql = "select product_id, product_name, category, image_url, price, stock, description, "+
-                "created_date, last_modified_date from product";
+                "created_date, last_modified_date from product where 1 = 1";
         Map<String, Object> map = new HashMap<>();
+        if(category != null){
+            System.out.println("H!!!!!");
+            sql = sql + " AND category = :category";
+            map.put("category",category.name());
+        }
+
+        if(search != null){
+            sql = sql + " and product_name like :search";
+            map.put("search","%"+search + "%");
+        }
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
 
